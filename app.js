e82e844887dc27e2c7c94fe6970d99985a1ce308
zakaria <zgejadze@gmail.com>
2022-06-23 00:26:22 +0400

    added csrf protection and error handling it crashes need work on sessions

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-const path = require('path')  
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const csrf = require("csurf");
 
-const db = require('./data/database');
+const db = require("./data/database");
+const addCsrfTokenMiddlware = require('./middlewares/csrf-token')
+const authRoutes = require("./routes/auth.routes"); //routes related to authentification
 
-const authRoutes = require('./routes/auth.routes')  //routes related to authentification
+const errorHandlerMiddleware = require('./middlewares/error-handler')
 
-const app = express()
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-
-app.use(express.static('public'));
-
-app.use(authRoutes)
+const app = express();
 
 
-db.conectToDatabase().then(function (){
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+//protection against csrf attacks
+app.use(csrf());
+app.use(addCsrfTokenMiddlware) //  middleware to add csrf token to locals 
+
+
+app.use(authRoutes);
+
+
+app.use(errorHandlerMiddleware);
+db.conectToDatabase()
+  .then(function () {
     app.listen(3000);
-}).catch(function(error){
-    console.log('failed to connect to the database!')
-    console.log(error)
-})
+  })
+  .catch(function (error) {
+    console.log("failed to connect to the database!");
+    console.log(error);
+  });
