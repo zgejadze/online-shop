8c8f4c4a3e7aaffeba40f685e9ebb919d2390873
zakaria <zgejadze@gmail.com>
2022-07-02 23:58:32 +0400

    added product editing functionality

--- models/product.model.js
+++ models/product.model.js
@@ -1,44 +1,81 @@
 const db = require("../data/database");
-
-
-
+const mongodb = require("mongodb");
 
 class Product {
-    constructor(productData) {
-        this.title = productData.title;
-        this.summary = productData.summary;
-        this.price = +productData.price;
-        this.description = productData.description;
-        this.image = productData.image;  // this is image name
-        this.imagePath = `product-data/images/${productData.image}`
-        this.imageUrl = `/product/assets/images/${productData.image}`
-        if(productData._id){
-            this.id = productData._id.toString();
-        }
+  constructor(productData) {
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price;
+    this.description = productData.description;
+    this.image = productData.image; // this is image name
+    this.updateImageData();
+    if (productData._id) {
+      this.id = productData._id.toString();
+    }
+  }
+
+  static async findById(productId) {
+    let prodId;
+    try {
+      prodId = new mongodb.ObjectId(productId);
+    } catch (error) {
+      error.code = 404;
+      throw error;
+    }
+    const product = await db
+      .getDb()
+      .collection("products")
+      .findOne({ _id: prodId });
+
+    if (!product) {
+      const error = new Error("Could not find product with provided Id.");
+      error.code = 404;
+      throw error;
     }
 
-    static async findAll() {
-        const products = await db.getDb().collection('products').find().toArray();
+    return new Product(product);
+  }
 
+  static async findAll() {
+    const products = await db.getDb().collection("products").find().toArray();
 
-        return products.map(function (product){
-            return new Product(product)
-        })
+    return products.map(function (product) {
+      return new Product(product);
+    });
+  }
+
+  updateImageData() {
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/product/assets/images/${this.image}`;
+  }
+
+  async save() {
+    const productData = {
+      title: this.title,
+      summary: this.summary,
+      price: this.price,
+      description: this.description,
+      image: this.image,
+    };
+
+    if (this.id) {
+      const productId = mongodb.ObjectId(this.id);
+
+      if(!this.image){
+        delete productData.image;
+      }
+      await db
+        .getDb()
+        .collection("products")
+        .updateOne({ _id: productId }, { $set: productData });
+    } else {
+      await db.getDb().collection("products").insertOne(productData);
     }
-
-
-    async save() {
-        const productData = {
-            title: this.title,
-            summary: this.summary,
-            price: this.price,
-            description: this.description,
-            image: this.image
-        }
-
-        await db.getDb().collection('products').insertOne(productData);
-    }
+  }
+  async replaceImage(newImage) {
+    this.image = newImage
+    this.updateImageData();
+  }
 }
 
-
-module.exports = Product
+module.exports = Product;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-const adminController = require('../controllers/admin.controller')
+const adminController = require("../controllers/admin.controller");
 
-const imageUploadMiddleware = require('../middlewares/image-upload')
+const imageUploadMiddleware = require("../middlewares/image-upload");
 
 const router = express.Router();
 
-router.get('/products', adminController.getProducts)
+router.get("/products", adminController.getProducts);
 
-router.get('/products/new', adminController.getNewProduct)
+router.get("/products/new", adminController.getNewProduct);
 
-router.post('/products', imageUploadMiddleware, adminController.createNewProduct)
+router.post(
+  "/products",
+  imageUploadMiddleware,
+  adminController.createNewProduct
+);
 
+router.get("/products/:id", adminController.getUpdateProduct);
+
+router.post("/products/:id", imageUploadMiddleware, adminController.updateProduct);
 
 module.exports = router;
