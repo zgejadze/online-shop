0e25c17132dec907fb3eb0e17e93860bcf749775
zakaria <zgejadze@gmail.com>
2022-07-05 21:14:40 +0400

    added cart page

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const cartController = require('../controllers/cart.controller')
 
-const router = express.Router();
+const router = express.Router(); // all routes here start with /cart
+
+router.get('/', cartController.getCart)
 
 router.post('/items', cartController.addCartItem)
 
